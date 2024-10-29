0f5ef88e5cc876b8244c84951532ba894781e12c
FrederickKumah <fdblay@gmail.com>
2024-10-29 11:10:08 +0000

    added userSchema

---
+++ models/user.js
@@ -0,0 +1,55 @@
+import { Schema, model } from "mongoose";
+import { toJSON } from "@reis/mongoose-to-json";
+
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    phone: {
+        type: String,
+        required: true
+    },
+
+    personalID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    role: {
+        type: String,
+        default: 'user',
+        enum: [
+            'user',
+            'freelancer',
+            'manager',
+            'admin'
+        ]
+    }
+}, {
+    timestamps: true,
+});
+
+userSchema.plugin(toJSON);
+
+export const UserModel = model('User', userSchema);
