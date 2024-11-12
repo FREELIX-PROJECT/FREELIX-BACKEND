1a280b2e99f9e54b32da1864dfa96d53f5b4a816
FrederickKumah <fdblay@gmail.com>
2024-11-12 11:08:06 +0000

    made changes in the user model

--- validators/task.js
+++ validators/task.js
@@ -3,15 +3,16 @@ import Joi from "joi";
 export const addTaskValidator = Joi.object({
     title: Joi.string().required(),
     description: Joi.string(),
-    assignedTo: Joi.string().required(),
-    hoursLogged: Joi.number()
+    status: Joi.string()
+    // assignedTo: Joi.string().required(),
+    // hoursLogged: Joi.number()
 });
 
 
 export const updateTaskValidator = Joi.object({
     title: Joi.string(),
     description: Joi.string(),
-    assignedTo: Joi.string(),
-    status: Joi.string(),
-    hoursLogged: Joi.number()
+    status: Joi.string()
+    // assignedTo: Joi.string(),
+    // hoursLogged: Joi.number()
 });
