4f145d48dc4bd9df2c642df4cca7ff2542f10a2c
fdblay <67843349+fdblay@users.noreply.github.com>
2024-11-13 14:03:53 +0000

    updated task schema

--- validators/task.js
+++ validators/task.js
@@ -1,17 +1,18 @@
 import Joi from "joi";
 
 export const addTaskValidator = Joi.object({
-    title: Joi.string().required(),
     description: Joi.string(),
-    assignedTo: Joi.string().required(),
-    hoursLogged: Joi.number()
+    status: Joi.string()
+    // title: Joi.string().required(),
+    // assignedTo: Joi.string().required(),
+    // hoursLogged: Joi.number()
 });
 
 
 export const updateTaskValidator = Joi.object({
-    title: Joi.string(),
     description: Joi.string(),
-    assignedTo: Joi.string(),
-    status: Joi.string(),
-    hoursLogged: Joi.number()
+    status: Joi.string()
+    // title: Joi.string(),
+    // assignedTo: Joi.string(),
+    // hoursLogged: Joi.number()
 });
